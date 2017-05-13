3d21f1a63ce3bb71ef9ca6f32efc2e2167d4132e
Chao Lian <charleslian@126.com>
2017-05-13 16:22:08 +0800

    test phase

--- bin/espresso/phase.py
+++ bin/espresso/phase.py
@@ -10,9 +10,33 @@ import pyramids.io.result as dp
 import pyramids.plot.setting as ma
 import pyramids.plot.PlotUtility as ppu
 from pyramids.io.fdf import tdapOptions
+from matplotlib import pyplot as plt
+import numpy as np
 import os
 
 def readData(filename='silicon.phase.dat'):
   f = open(filename)
-  for line in f.readlines():
+  text = f.readlines()
+  nbnd, nkstot = [int(i) for i in text[0].split()]
+  
+  nstep = (len(text) - 1)/(nkstot+1)
+  print nbnd, nkstot, nstep
+  del text[::(nkstot+1)]
+  #print text
+  #data = np.zeros([nbnd,nkstot,nstep])
+  #data = 
+  data = np.array([[float(i) for i in line.split()] for line in text])
+  data = data[:nstep*nkstot].reshape([nstep, nkstot, nbnd])
+  #print data[0,0,:]
+  #[ for k in range(nkstot) for step in range(nstep)]
+  return data
+
     
+phase = readData()
+norm = readData('silicon.norm.dat')
+print norm.sum()
+
+#plt.plot(norm[:,:,:].sum(axis=2),'.')
+for ik in range(phase.shape[1]):
+    fig, axs = plt.subplots(1)
+    axs.plot(phase[:,ik,:],'-')
