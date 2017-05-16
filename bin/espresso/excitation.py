de0f7ecaf891ab662e31da30c980c727a21a23c8
Chao Lian <charleslian@126.com>
2017-05-16 16:41:54 +0800

    Current

--- bin/espresso/excitation.py
+++ bin/espresso/excitation.py
@@ -18,9 +18,11 @@ def readData(filename='silicon.phase.dat'):
   f = open(filename)
   text = f.readlines()
   nbnd, nkstot = [int(i) for i in text[0].split()]
+  kweight = [float(i) for i in text[1].split()]
   
-  nstep = (len(text) - 1)/(nkstot+1)
-  print nbnd, nkstot, nstep
+  nstep = (len(text) - 2)/(nkstot+1)
+  #print nbnd, nkstot, nstep
+  del text[1]
   del text[::(nkstot+1)]
   #print text
   #data = np.zeros([nbnd,nkstot,nstep])
@@ -29,10 +31,36 @@ def readData(filename='silicon.phase.dat'):
   data = data[:nstep*nkstot].reshape([nstep, nkstot, nbnd])
   #print data[0,0,:]
   #[ for k in range(nkstot) for step in range(nstep)]
-  return data
+  return data, kweight
 
     
-phase = readData()
-norm = readData('silicon.norm.dat')
-plt.plot((norm[:,:,:4] - norm[0,:,:4]).sum(axis=(1,2)))
-plt.plot((norm[:,:,4:]- norm[0,:,4:]).sum(axis=(1,2)))
+#phase = readData()
+
+
+fig, axs = plt.subplots(2,2,sharex=True,figsize=(10,6))
+axs = axs.flatten()
+norm, kweight = readData('silicon.norm.dat')
+axs[0].plot((norm[:,:,:4] - norm[0,:,:4]).sum(axis=(1,2)))
+axs[0].plot((norm[:,:,4:]- norm[0,:,4:]).sum(axis=(1,2)))
+args = ma.getPropertyFromPosition(0,title='Excited electrons',ylabel='n (e)')
+ma.setProperty(axs[0],**args)
+Efield = [[float(i) for i in line.split()] for line in open('TDEFIELD')]
+Efield = np.array(Efield)/1E5
+
+ppu.plotTotalEnergy(axs[2])
+ppu.plotEField(axs[1])
+
+f = os.popen('grep current result')
+current = np.array([[float(i) for i in line.split()[2:]] for line in f.readlines()])*13.6
+for idir in range(3):  
+    axs[3].plot(current[:,idir],label=['x','y','z'][idir])
+
+args = ma.getPropertyFromPosition(3,title='Current',ylabel='j (a.u.)')
+ma.setProperty(axs[3],**args)
+
+plt.tight_layout()
+
+SaveName = __file__.split('/')[-1].split('.')[0]
+for save_type in ['.png']:
+  filename = SaveName + save_type
+  plt.savefig(filename,orientation='portrait',dpi=600)
